087e488e002d8a44c7d5851f3c54258f7b3b1436
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2016-01-15 13:49:28 -0600

    Comclucion de Diseño de Formulario.

--- RegistroDeEstudiantesForm/RegistroDeEstudiantesForm.cs
+++ RegistroDeEstudiantesForm/RegistroDeEstudiantesForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RegistroDeEstudiantesForm
 {
-    public partial class Form1 : Form
+    public partial class RegistroDeEstudiantesForm : Form
     {
-        public Form1()
+        public RegistroDeEstudiantesForm()
         {
             InitializeComponent();
         }
